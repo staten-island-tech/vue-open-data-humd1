c48a49437d852daba2edefff260560b0db310948
humd1 <humdb@nycstudents.net>
2026-03-17 10:26:55 -0400

    llaalala

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class ="bg-blue-500 text-white p-4">TAILWIND</div>
 </template>
 
 <style scoped></style>
